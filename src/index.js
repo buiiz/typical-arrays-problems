5b6ebe35aae6f53f74055c5b2ba353551e5b2b3b
buiiz <ivantur2@gmail.com>
2020-09-29 17:42:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+exports.min = function min(arr) {
+    if (!arr || !arr.length) return 0;
+    return arr.sort((a, b) => a - b)[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(arr) {
+    if (!arr || !arr.length) return 0;
+    return arr.sort((a, b) => b - a)[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(arr) {
+    if (!arr || !arr.length) return 0;
+    return arr.reduce((sum, num) => sum + num, 0) / arr.length;
+};
